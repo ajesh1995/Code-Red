543d17770ed779f348437b3aba96d941ea0b21ca
root <root@ip-172-31-18-144.ec2.internal>
2023-05-18 15:00:11 +0000

    on the changes done inthe code

---
+++ providers.tf
@@ -0,0 +1,16 @@
+###PROVIDERS.TF
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = var.region_name
+}
+
+
